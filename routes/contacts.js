15b61cd7d9940f3f747fedd19bb475932b7064de
Justice Sefeane <justices@junkmail.co.za>
2020-03-26 10:23:31 +0200

    contacts add, put and delete setup

--- routes/contacts.js
+++ routes/contacts.js
@@ -91,7 +91,7 @@ router.put('/:id', auth, async (req, res) => {
     if(!contact) return res.status(404).json({ msg: 'Contact not found' });
 
     // Make sure user owns contacts
-    if(contact.user.toString !== req.user.id) {
+    if(contact.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'Not Authorised' })
     }
 
@@ -109,8 +109,25 @@ router.put('/:id', auth, async (req, res) => {
 // @route    DELETE api/contacts/:id
 // @desc     Delete contact
 // @access   Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete contact');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if(!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+    // Make sure user owns contacts
+    if(contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not Authorised' })
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+
+    res.json({ msg: 'Contact Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
 });
 
 module.exports = router;
