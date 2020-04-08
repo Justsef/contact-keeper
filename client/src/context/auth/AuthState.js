94f51cdda977910b0adab37b1ad7bb7880fb3081
Justice Sefeane <justices@junkmail.co.za>
2020-04-08 11:46:07 +0200

    Connect Login and register global state to server and database

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -25,7 +26,24 @@ const AuthState = props => {
   const [state, dispatch] = useReducer(authReducer, initialState)
 
   // Load User
-  const loadUser = () => console.log('loaduser');
+  const loadUser = async () => {
+    if(localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    try {
+      const res = await axios.get('/api/auth');
+
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      })
+    } catch (err) {
+      dispatch({ type: AUTH_ERROR })
+
+    }
+  };
+
 
   // Register User
   const register = async formData => {
@@ -33,7 +51,7 @@ const AuthState = props => {
       headers: {
         'Content-Type': 'application/json'
       }
-    }
+    };
 
     try {
       const res = await axios.post('/api/users', formData, config);
@@ -42,6 +60,8 @@ const AuthState = props => {
         type: REGISTER_SUCCESS,
         payload: res.data
       })
+
+      loadUser();
     } catch (err) {
       dispatch({
         type: REGISTER_FAIL,
@@ -50,9 +70,31 @@ const AuthState = props => {
     }
   }
   // Login User
-  const login = () => console.log('loginuser');
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
   // Logout
-  const logout = () => console.log('logout');
+  const logout = () => dispatch({ type: LOGOUT })
   // Clear Errors
   const clearErrors = () => dispatch({type: CLEAR_ERRORS});
 
